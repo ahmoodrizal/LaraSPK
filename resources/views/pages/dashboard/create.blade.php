ce9511be1db3820f7fbc217c4d49aaecf8dc802a
AhmadRizal <amdrizalc@gmail.com>
2022-05-14 19:41:07 +0700

    fix create project form

--- resources/views/pages/dashboard/create.blade.php
+++ resources/views/pages/dashboard/create.blade.php
@@ -4,8 +4,8 @@
     <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
         Buat Studi Kasus
     </h2>
-    <div class="w-full overflow-hidden rounded-lg shadow-xs">
-        <div class="w-2/3 overflow-x-auto">
+    <div class="w-full lg:w-2/4 overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
             <form action="{{ route('dashboard.store') }}" method="post">
                 @csrf
                 <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
